49f5be8c5c0d9fab652cb40b7b649e9ba9f231ce
opeloyeru <opeloyerun@gmail.com>
2022-10-18 00:38:38 -0700

    first commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+
+/**
+ *main-print the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
