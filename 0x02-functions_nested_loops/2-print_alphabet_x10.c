2a6ddae8bba7e97f2bd77749ec00f9e52aa65edc
opeloyeru <opeloyerun@gmail.com>
2022-10-30 10:33:54 -0700

    c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -16,10 +16,10 @@ void print_alphabet_x10(void)
 	letter = 'a';
 	while (letter <= 'z')
 	{
-	_putchar(letter)
+	_putchar(letter);
 	letter++;
 	}
-	_putchar('\n')
+	_putchar('\n');
 
 	round++;
 	}
