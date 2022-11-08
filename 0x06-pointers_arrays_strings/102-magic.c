8a123c1149d6d583e636a6c995029d5cf9cedc8f
opeloyeru <opeloyerun@gmail.com>
2022-11-08 13:13:08 -0800

    102

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main -Entry point
+ *
+ * A dream doesn't become reality through magic; it takes sweat, determination and hard work
+ *
+ * Return: Always 0 (Success)
+ 
 int main(void)
 {
 	int n;
